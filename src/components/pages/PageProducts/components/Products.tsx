f4323f37fa9eeb69027ca1b81752e06230e98841
Dmitriy <krudda@yandex.ru>
2023-02-16 15:10:47 +0300

    feat - add loader

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -2,7 +2,9 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import CircularProgress from "@mui/material/CircularProgress";
 import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import { formatAsPrice } from "~/utils/utils";
 import AddProductToCart from "~/components/AddProductToCart/AddProductToCart";
@@ -12,21 +14,30 @@ export default function Products() {
   const { data = [], isLoading } = useAvailableProducts();
 
   if (isLoading) {
-    return <Typography>Loading...</Typography>;
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "545px",
+        }}
+      >
+        <CircularProgress sx={{ color: "#ffe490" }} />
+      </Box>
+    );
   }
 
   return (
     <Grid container spacing={4}>
       {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
       {data.map((product) => {
-        console.log("product", product);
         return (
           <Grid item key={product.id} xs={12} sm={6} md={4}>
             <Card
               sx={{ height: "545px", display: "flex", flexDirection: "column" }}
             >
               <CardMedia
-                // sx={{ pt: "56.25%" }}
                 sx={{ height: "445px" }}
                 image={product.poster}
                 title="Image title"
