6bec52eb38a227ae6f40b2137a05e8ea144928e0
Dmitriy <krudda@yandex.ru>
2023-03-11 22:09:05 +0300

    feat - add authorization heder and access errors handling

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import axios from "axios";
+import { response } from "msw";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +20,26 @@ if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
   worker.start({ onUnhandledRequest: "bypass" });
 }
+
+axios.interceptors.response.use(
+  (response) => {
+    console.log("response in axios.interceptors", JSON.stringify(response));
+    return response;
+  },
+  (error) => {
+    console.log("error in axios.interceptors", error);
+
+    const errorResponse = error.response;
+    if (errorResponse.status === 400) {
+      alert(errorResponse.data?.data);
+    }
+
+    if (errorResponse.status === 401 || errorResponse.status === 403) {
+      alert(errorResponse.data?.message);
+    }
+    return Promise.reject(error?.response ?? error);
+  }
+);
 
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
